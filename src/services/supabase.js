98ac1004e8e6a4a55d00ad133951b7ab60f7ca55
Aleksandër Pulla <aleksanderpulla@gmail.com>
2023-08-31 21:48:53 +0200

    Updated environment variables

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,8 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabaseUrl = "https://iwzxglvmdthyhkrowudi.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml3enhnbHZtZHRoeWhrcm93dWRpIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTIwNDA3MzgsImV4cCI6MjAwNzYxNjczOH0.SuWwM9dcWKJuufGf_-hAX7FiFIh66cvRcMREvo2N9ek";
+export const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
